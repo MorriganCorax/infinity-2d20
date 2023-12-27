f5392fb3c638c1b5a754af7a8c5195a3472d5a09
MorriganCorax <arkanik_queen@hotmail.com>
2023-12-27 13:30:06 +0100

    Base build for NPC sheet

---
+++ module/helpers/config.mjs
@@ -0,0 +1,25 @@
+export const INFINITY2D20 = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+INFINITY2D20.abilities = {
+  "agi": "INFINITY2D20.AbilityAgi",
+  "awa": "INFINITY2D20.AbilityAwa",
+  "brw": "INFINITY2D20.AbilityBrw",
+  "coo": "INFINITY2D20.AbilityCoo",
+  "int": "INFINITY2D20.AbilityInt",
+  "per": "INFINITY2D20.AbilityPer",
+  "wil": "INFINITY2D20.AbilityWil"
+};
+
+INFINITY2D20.abilityAbbreviations = {
+  "agi": "INFINITY2D20.AbilityAgiAbbr",
+  "awa": "INFINITY2D20.AbilityAwaAbbr",
+  "brw": "INFINITY2D20.AbilityBrwAbbr",
+  "coo": "INFINITY2D20.AbilityCooAbbr",
+  "int": "INFINITY2D20.AbilityIntAbbr",
+  "per": "INFINITY2D20.AbilityPerAbbr",
+  "wil": "INFINITY2D20.AbilityWilAbbr"
+};
